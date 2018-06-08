113516f64602560425583b11f69df26a1d4f6b7b
mda14 <Chispi@dyn3157-237.wlan.ic.ac.uk>
2018-06-08 15:16:32 +0100

    fix

--- dog_features_extraction/__init__.py
+++ dog_features_extraction/__init__.py
@@ -212,7 +212,7 @@ def featureSelection_pca(features, title):
     #plt.xlim([0, 29]);
     plt.legend(loc='best',fontsize = 18);
 
-    plt.savefig("featureSelection_pca_",title)
+    plt.savefig("featureSelection_pca_"+title)
 
 def featureSelection_2d_visualise(features, y_features, title):
     pca = PCA(n_components = 2)
@@ -234,5 +234,5 @@ def featureSelection_2d_visualise(features, y_features, title):
                    , s = 50)
     ax.legend(targets)
     ax.grid()
-    plt.savefig("2d_pca_",title)
+    plt.savefig("2d_pca_"+title)
     return trans;
